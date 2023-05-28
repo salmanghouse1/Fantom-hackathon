ab19330283f528fe2a1d40f5f2d0c65be7219e7f
salman ghouse <salmanghouse1@gmail.com>
2023-05-28 19:51:21 -0400

    added some table components to market place and flex layouts

--- client/src/pages/Marketplace.js
+++ client/src/pages/Marketplace.js
@@ -14,10 +14,11 @@ function MarketPlace() {
     return ( 
         
         
-        <div className='columns-3 container mx-auto'>
-           
+        <div>
+           <div style={{display:'flex'}}>
+
    
-                <div className="divTableBody w-full aspect-video">
+                <div style={{flex:'300px'}}>
 <div class="divTableRow">
 <div class="divTableCell">Token Name</div>
 <div class="divTableCell">Price</div>
@@ -62,7 +63,7 @@ function MarketPlace() {
 
 </div>
 
-<div id="GraphContainer" className="tGraphComp w-full aspect-video">
+<div style={{flex:'300px'}}>
 
             <ApexChartGraphComp/>
 
@@ -70,8 +71,99 @@ function MarketPlace() {
           
           
       
-<div className='w-full aspect-video'></div>
-     </div>
+<div style={{flex:'300px'}}>
+<div class="divTableRow">
+<div class="divTableCell">Price</div>
+<div class="divTableCell">Amount</div>
+<div class="divTableCell">Total</div>
+</div>
+{/* The Rows bellow needs to be mapped, the smart contract or thirdweb sdk needs to getWallets */}
+<div class="divTableRow">
+<div class="divTableCell">&nbsp;</div>
+<div class="divTableCell">&nbsp;</div>
+<div class="divTableCell">&nbsp;</div>
+</div>
+<div class="divTableRow">
+<div class="divTableCell">&nbsp;</div>
+<div class="divTableCell">&nbsp;</div>
+<div class="divTableCell">&nbsp;</div>
+</div>
+<div class="divTableRow">
+<div class="divTableCell">&nbsp;</div>
+<div class="divTableCell">&nbsp;</div>
+<div class="divTableCell">&nbsp;</div>
+</div>
+<div class="divTableRow">
+<div class="divTableCell">&nbsp;</div>
+<div class="divTableCell">&nbsp;</div>
+<div class="divTableCell">&nbsp;</div>
+</div>
+<div class="divTableRow">
+<div class="divTableCell">&nbsp;</div>
+<div class="divTableCell">&nbsp;</div>
+<div class="divTableCell">&nbsp;</div>
+</div>
+<div class="divTableRow">
+<div class="divTableCell">&nbsp;</div>
+<div class="divTableCell">&nbsp;</div>
+<div class="divTableCell">&nbsp;</div>
+</div>
+<div class="divTableRow">
+<div class="divTableCell">&nbsp;</div>
+<div class="divTableCell">&nbsp;</div>
+<div class="divTableCell">&nbsp;</div>
+</div>
+</div>
+
+           </div>
+<div style={{display:'flex'}}>
+    
+<div style={{flex:'300px'}}>
+<div class="divTableRow">
+<div class="divTableCell">Price</div>
+<div class="divTableCell">Amount</div>
+<div class="divTableCell">Time</div>
+</div>
+{/* The Rows bellow needs to be mapped, the smart contract or thirdweb sdk needs to getWallets */}
+<div class="divTableRow">
+<div class="divTableCell">&nbsp;</div>
+<div class="divTableCell">&nbsp;</div>
+<div class="divTableCell">&nbsp;</div>
+</div>
+<div class="divTableRow">
+<div class="divTableCell">&nbsp;</div>
+<div class="divTableCell">&nbsp;</div>
+<div class="divTableCell">&nbsp;</div>
+</div>
+<div class="divTableRow">
+<div class="divTableCell">&nbsp;</div>
+<div class="divTableCell">&nbsp;</div>
+<div class="divTableCell">&nbsp;</div>
+</div>
+<div class="divTableRow">
+<div class="divTableCell">&nbsp;</div>
+<div class="divTableCell">&nbsp;</div>
+<div class="divTableCell">&nbsp;</div>
+</div>
+<div class="divTableRow">
+<div class="divTableCell">&nbsp;</div>
+<div class="divTableCell">&nbsp;</div>
+<div class="divTableCell">&nbsp;</div>
+</div>
+<div class="divTableRow">
+<div class="divTableCell">&nbsp;</div>
+<div class="divTableCell">&nbsp;</div>
+<div class="divTableCell">&nbsp;</div>
+</div>
+<div class="divTableRow">
+<div class="divTableCell">&nbsp;</div>
+<div class="divTableCell">&nbsp;</div>
+<div class="divTableCell">&nbsp;</div>
+</div>
+
+</div>
+<div style={{flex:'300px'}}>b</div><div style={{flex:'300px'}}> c</div></div>
+</div> 
         
      );
 }
